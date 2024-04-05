79b904a23f5e6683c5243ede1885956ce0d4f53e
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-04-06 01:34:42 +0700

    update vnpay

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68b57e0f27ae5dc854ac236c490b9f2cc7aed2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc4c685d555062ceef6201691d3ee7af987b9c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
