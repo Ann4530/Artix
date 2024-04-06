18b31812d8f7e8d63d28357c3e29f643e2ccd070
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-04-06 09:20:09 +0700

    assembly

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681d2a5d9054aba08059d3df7081ce47472d47f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+689d83eeae4deef7f63ebed34b232309fd937a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
